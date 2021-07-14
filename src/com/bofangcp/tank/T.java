ee940cb83fdede480288d2c9662d75606914e9cc
搬码道人 <49650197+MapleLeafSong@users.noreply.github.com>
2021-07-14 22:50:55 +0800

    tank
    初始化窗口

---
+++ src/com/bofangcp/tank/T.java
@@ -0,0 +1,38 @@
+package com.bofangcp.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * @Author:GangleiSong
+ * @Date:Create in 2021/7/14 21:52
+ **/
+public class T {
+
+    public static void main(String[] args) {
+        //代表的就是一个窗口
+        Frame f = new Frame();
+        //设置可将
+        f.setSize(800,600);
+        f.setResizable(false);
+        f.setTitle("tank war");
+        f.setVisible(true);
+
+
+
+        f.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+
+    }
+
+
+
+
+
+}
