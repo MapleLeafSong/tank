9d3922ad76e19bad5968adfacb11c921c3c6887f
搬码道人 <49650197+MapleLeafSong@users.noreply.github.com>
2021-07-14 23:33:17 +0800

    继承一个类

---
+++ src/com/bofangcp/tank/TankFrame.java
@@ -0,0 +1,20 @@
+package com.bofangcp.tank;
+
+import java.awt.*;
+
+/**
+ * @Author:GangleiSong
+ * @Date:Create in 2021/7/14 23:17
+ **/
+public class TankFrame extends Frame {
+
+    public TankFrame(){
+
+    }
+
+
+
+
+
+
+}
