45896753dc77c4235aacaef6f68c5485b4e0505f
搬码道人 <49650197+MapleLeafSong@users.noreply.github.com>
2021-07-14 23:46:38 +0800

    开始paint

--- src/com/bofangcp/tank/T.java
+++ src/com/bofangcp/tank/T.java
@@ -11,22 +11,25 @@ import java.awt.event.WindowEvent;
 public class T {
 
     public static void main(String[] args) {
-        //代表的就是一个窗口
-        Frame f = new Frame();
-        //设置可将
-        f.setSize(800,600);
-        f.setResizable(false);
-        f.setTitle("tank war");
-        f.setVisible(true);
+//        //代表的就是一个窗口
+//        Frame f = new Frame();
+//        //设置可将
+//        f.setSize(800,600);
+//        f.setResizable(false);
+//        f.setTitle("tank war");
+//        f.setVisible(true);
+//
+//
+//
+//        f.addWindowListener(new WindowAdapter() {
+//            @Override
+//            public void windowClosing(WindowEvent e) {
+//                System.exit(0);
+//            }
+//        });
 
 
-
-        f.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        TankFrame tf = new TankFrame();
 
 
     }
--- src/com/bofangcp/tank/TankFrame.java
+++ src/com/bofangcp/tank/TankFrame.java
@@ -1,6 +1,8 @@
 package com.bofangcp.tank;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * @Author:GangleiSong
@@ -10,11 +12,21 @@ public class TankFrame extends Frame {
 
     public TankFrame(){
 
+        setSize(800,600);
+        setResizable(false);
+        setTitle("坦克大战");
+        setVisible(true);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
     }
 
-
-
-
-
-
+    @Override
+    public void paint(Graphics g) {
+        System.out.println("paint");
+    }
 }
